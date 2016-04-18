43a5419596c5fd9aa9793e5578fe25131f641264
JHKoopman <j.koopman@jarict.nl>
2016-04-18 11:16:43 +0200

    Added constraints UI and back end for Contact Me View Controller

--- Jari Koopman/ContactMeViewController.swift
+++ Jari Koopman/ContactMeViewController.swift
@@ -7,8 +7,9 @@
 //
 
 import UIKit
+import MessageUI
 
-class ContactMeViewController: UIViewController {
+class ContactMeViewController: UIViewController, MFMailComposeViewControllerDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,15 +28,45 @@ class ContactMeViewController: UIViewController {
         toggleSideMenuView()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func twitterPressed() {
+        let url: NSURL = NSURL(string: "https://twitter.com/lordpotvis")!
+        UIApplication.sharedApplication().openURL(url)
     }
-    */
 
+    @IBAction func facebookPressed() {
+        let url: NSURL = NSURL(string: "https://www.facebook.com/hans.koopman.7169?ref=bookmarks")!
+        UIApplication.sharedApplication().openURL(url)
+    }
+    
+    @IBAction func phonePressed() {
+        let phoneNumber = "tel://0031591362764"
+        let url: NSURL = NSURL(string: phoneNumber)!
+        UIApplication.sharedApplication().openURL(url)
+    }
+
+    @IBAction func mailPressed() {
+        sendMail()
+    }
+    
+    //MARK: Mail Setup
+    
+    func sendMail() {
+        if MFMailComposeViewController.canSendMail() {
+            let mail = MFMailComposeViewController()
+            mail.mailComposeDelegate = self
+            mail.setToRecipients(["j.koopman@jarict.nl"])
+            self.presentViewController(mail, animated: true, completion: nil)
+        } else {
+            let failedMailController = UIAlertController(title: "Unable to send mail!", message: "Unable to send mail, please try again!", preferredStyle: .Alert)
+            let okButton = UIAlertAction(title: "OK", style: .Default, handler: nil)
+            failedMailController.addAction(okButton)
+            
+            presentViewController(failedMailController, animated: true, completion: nil)
+        }
+        
+    }
+    
+    func mailComposeController(controller: MFMailComposeViewController, didFinishWithResult result: MFMailComposeResult, error: NSError?) {
+        controller.dismissViewControllerAnimated(true, completion: nil)
+    }
 }
