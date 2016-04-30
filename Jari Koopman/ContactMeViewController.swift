49ed04a2ba25ee23164e08811f7ac96db4140548
Jari Koopman <MBJ@macbook-pro-van-jari.local>
2016-04-30 20:37:05 +0200

    Added MapKit functionality

--- Jari Koopman/ContactMeViewController.swift
+++ Jari Koopman/ContactMeViewController.swift
@@ -8,20 +8,52 @@
 
 import UIKit
 import MessageUI
+import MapKit
+
+
+class MapPin : NSObject, MKAnnotation {
+    var coordinate: CLLocationCoordinate2D
+    var title: String?
+    
+    init(coordinate: CLLocationCoordinate2D, title: String) {
+        self.coordinate = coordinate
+        self.title = title
+    }
+}
 
 class ContactMeViewController: UIViewController, MFMailComposeViewControllerDelegate {
+    @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var showOtherButton: UIButton!
+    
+    let myLocation = CLLocation(latitude: 52.81701279, longitude: 6.92053661)
+    let mySchoolLocation = CLLocation(latitude: 52.511577, longitude: 6.100937)
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = UIColor.lightGrayColor()
-
+        let myHomeCoordinates: CLLocationCoordinate2D = CLLocationCoordinate2D(latitude: 52.81701279, longitude: 6.92053661)
+        let mySchoolCoordinates: CLLocationCoordinate2D = CLLocationCoordinate2D(latitude: 52.511577, longitude: 6.100937)
+        let myHome: MapPin = MapPin(coordinate: myHomeCoordinates, title: "My home!")
+        let mySchool: MapPin = MapPin(coordinate: mySchoolCoordinates, title: "My school!")
+        
         self.title = "Contact me"
         
         navigationItem.leftBarButtonItem = UIBarButtonItem(image: UIImage(named: "menu"), style: .Done, target: self, action: #selector(ContactMeViewController.openSideMenu))
+        centerMapOnLocation(myLocation)
+        mapView.addAnnotation(myHome)
+        mapView.addAnnotation(mySchool)
     }
+    
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+    }
+    
+    let regionRadius: CLLocationDistance = 1000
+    func centerMapOnLocation(location: CLLocation) {
+        let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate, regionRadius * 2.0, regionRadius * 2.0)
+        mapView.setRegion(coordinateRegion, animated: true)
     }
     
     func openSideMenu(){
@@ -46,6 +78,15 @@ class ContactMeViewController: UIViewController, MFMailComposeViewControllerDele
 
     @IBAction func mailPressed() {
         sendMail()
+    }
+    @IBAction func showOther(sender: AnyObject) {
+        if showOtherButton.titleLabel?.text == "Show school" {
+            centerMapOnLocation(mySchoolLocation)
+            showOtherButton.setTitle("Show home", forState: .Normal)
+        } else {
+            centerMapOnLocation(myLocation)
+            showOtherButton.setTitle("Show school", forState: .Normal)
+        }
     }
     
     //MARK: Mail Setup
